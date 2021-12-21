750909132e1ebc40682f7e8a10aef65b7e7efa0d
Dalyn Boyd <dalyn.boyd@gmail.com>
2021-12-21 13:17:45 -0600

    fix bing to bind

--- WebXR-Udemy/start/lecture2_2/app.js
+++ WebXR-Udemy/start/lecture2_2/app.js
@@ -6,18 +6,27 @@ class App{
 		const container = document.createElement( 'div' );
 		document.body.appendChild( container );
     
+		// Camera
+		// field of view, aspect ratio of rendered view, near plane, far plane
 		this.camera = new THREE.PerspectiveCamera( 60, window.innerWidth/window.innerHeight, 0.1, 100);
+		// move camera back to 4 in the Z
 		this.camera.position.set(0,0,4);
 
+		// Scene
 		this.scene = new THREE.Scene();
 		this.scene.background = new THREE.Color( 0xaaaaaa );
 
+		// Renderer
+		// need antialiasing to precent jagged edges
 		this.renderer = new THREE.WebGLRenderer( {antialias: true});
+		// important so things are blurry
 		this.renderer.setPixelRatio(window.devicePixelRatio);
+		// filling the window
 		this.renderer.setSize(window.innerWidth, window.innerHeight);
+		// domElement is created
 		container.appendChild(this.renderer.domElement);
-
-		this.renderer.setAnimationLoop(this.render.bing(this));
+		
+		this.renderer.setAnimationLoop(this.render.bind(this));
 
         window.addEventListener('resize', this.resize.bind(this) );
 	}	
